6b2896ebeda331f93a98aaf957163b76bbc0d1e5
Martin Naumoski <jajonaumoski@gmail.com>
2025-02-17 21:34:54 +0100

    finished, only media queries left

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 // Stars animation
 const n_stars = 150;
-const colors = ['#176ab6', '#fb9b39'];
-for (let i = 0; i < 98; i++) colors.push('#fff');
+const colors = ["#176ab6", "#fb9b39"];
+for (let i = 0; i < 98; i++) colors.push("#fff");
 
-const canvas = document.querySelector('canvas');
+const canvas = document.querySelector("canvas");
 canvas.width = innerWidth;
 canvas.height = innerHeight;
-const c = canvas.getContext('2d');
+const c = canvas.getContext("2d");
 
-canvas.style.background = '#000';
+canvas.style.background = "#000";
 
 const randomInt = (max, min) => Math.floor(Math.random() * (max - min) + min);
 
-addEventListener('resize', () => {
+addEventListener("resize", () => {
   canvas.width = innerWidth;
   canvas.height = innerHeight;
   stars = [];
@@ -61,7 +61,7 @@ const init = () => {
 
 const animate = () => {
   c.clearRect(0, 0, canvas.width, canvas.height);
-  
+
   const bg = c.createRadialGradient(
     canvas.width / 2,
     canvas.height / 2,
@@ -70,26 +70,26 @@ const animate = () => {
     canvas.height / 2,
     canvas.height * 2
   );
-  
-  bg.addColorStop(0, '#32465E');
+
+  bg.addColorStop(0, "#32465E");
   bg.addColorStop(0.4, "#000814");
   bg.addColorStop(0.8, "#000814");
   bg.addColorStop(1, "#000");
   c.fillStyle = bg;
   c.fillRect(0, 0, canvas.width, canvas.height);
 
-  stars.forEach(star => star.update(stars));
+  stars.forEach((star) => star.update(stars));
   requestAnimationFrame(animate);
 };
 
 init();
 animate();
 
-// Handlig form animation / about animation
-window.addEventListener('DOMContentLoaded', () => {
-  const contactText = document.querySelector('.contact-me-text');
-  const formContainer = document.querySelector('.container-contact-form-flex');
-  
+// Handlig form animation / about animation / projects animation
+window.addEventListener("DOMContentLoaded", () => {
+  const contactText = document.querySelector(".contact-me-text");
+  const formContainer = document.querySelector(".container-contact-form-flex");
+
   if (!contactText || !formContainer) {
     console.error("Missing elements for the contact form or loading text");
     return;
@@ -100,19 +100,20 @@ window.addEventListener('DOMContentLoaded', () => {
 
   const dotInterval = setInterval(() => {
     dotCount = (dotCount + 1) % (maxDots + 1);
-    contactText.textContent = `form loading${'.'.repeat(dotCount)}`;
+    contactText.textContent = `form loading${".".repeat(dotCount)}`;
   }, 1000);
 
   setTimeout(() => {
-    contactText.classList.add('loading-text');
+    contactText.classList.add("loading-text");
     clearInterval(dotInterval);
     setTimeout(() => {
-      contactText.style.display = 'none';
-      formContainer.style.display = 'block';
+      contactText.style.display = "none";
+      formContainer.style.display = "block";
     }, 2500);
   }, 3000);
+
+  if (!header || !cards) {
+    console.error("header or card conatiner not found");
+    return;
+  }
 });
-
-
-
-
