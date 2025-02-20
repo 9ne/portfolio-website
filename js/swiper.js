eb52bdf0765272f8970091c58ba5683a08815463
Martin Naumoski <jajonaumoski@gmail.com>
2025-02-20 21:17:52 +0100

    finished, media, queries left

---
+++ js/swiper.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const swiperContainer = document.querySelector(".swiper");
+  if (!swiperContainer) return;
+
+  const swiper = new Swiper(swiperContainer, {
+    direction: "horizontal",
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+});
